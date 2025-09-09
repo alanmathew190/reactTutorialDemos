c808f37498a29753efb4840824211685ae1edd09
Alan <alanmathew190@gmail.com>
2025-09-09 15:50:27 +0530

    final

--- assignment1/src/components/ProfileCard.jsx
+++ assignment1/src/components/ProfileCard.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 // import './profilecard.css'
 
-function ProfileCard({persons}) {
+function ProfileCard({name,img,description}) {
     return (
     
-        <div className="profile_card">
-            {persons.map((person, i) =>
-            <div  key={i}>
-                <img src={person.img} alt={name} />
-                <h1>{person.name}</h1>
-                    <p>{persons.description}</p>
-                    </div>
-                )}
+            <div className="profile_card">
+                <img src={img} alt={name} />
+                <h1>{name}</h1>
+                <p>{description}</p>
             </div>
             
   )
